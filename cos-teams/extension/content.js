2defb1c1069816015ebcc19f5266ab2209edf7d9
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-05-04 15:42:17 +0530

    refactor: gracefully remove cos-teams/extension/content.js to clean up repository

--- cos-teams/extension/content.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const token = localStorage.getItem('cos_teams_member_token');
-  if (token && typeof chrome !== 'undefined' && chrome.runtime?.id) {
-    chrome.runtime.sendMessage({ type: 'SET_MEMBER_TOKEN', token });
-  }
-})();
